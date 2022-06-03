24ae3b165c93053343dfe056862ead7767e19414
Mikhail Rak <misha.rak.2401@gmail.com>
2022-06-03 13:09:49 +0300

    override method calcBonus

--- src/app/MerchantA.java
+++ src/app/MerchantA.java
@@ -1,6 +1,6 @@
 package app;
 
-public class MerchantA extends Merchant {
+public class MerchantA extends Merchant implements Bonus{
 
     String email;
 
@@ -13,5 +13,16 @@ public class MerchantA extends Merchant {
         String name = getName();
         String phone = getPhone();
         return "\nMerchant: " + name + ", " + phone + ", " + email + "\n";
+    }
+
+    @Override
+    public double calcBonus(double sales) {
+        if (sales <= 10000) {
+            return sales * 5 / 100;
+        } else if (sales > 10000 && sales <= 30000) {
+            return sales * 10 / 100;
+        } else {
+            return sales * 15 / 100;
+        }
     }
 }
