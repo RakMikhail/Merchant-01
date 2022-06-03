c0aa5d2e56b6f1b24e2e3f11cadfd577978bec6e
Mikhail Rak <misha.rak.2401@gmail.com>
2022-06-03 15:07:01 +0300

    add new class ProductA

---
+++ src/app/ProductA.java
@@ -0,0 +1,13 @@
+package app;
+
+public class ProductA extends Product{
+
+    int qnty;
+    double price;
+
+    public ProductA(String name, int qnty, double price) {
+        super(name);
+        this.qnty = qnty;
+        this.price = price;
+    }
+}
