613891274d3e637eb50cc7665243c01359717d9b
Mikhail Rak <misha.rak.2401@gmail.com>
2022-06-03 15:05:24 +0300

    add new method calcBonus

--- src/app/MerchantA.java
+++ src/app/MerchantA.java
@@ -1,10 +1,10 @@
 package app;
 
-public class MerchantA extends  Merchant {
+public class MerchantA extends Merchant implements Bonus {
 
     String email;
 
-    public MerchantA(String name,String surname, String phone, String email) {
+    public MerchantA(String name, String surname, String phone, String email) {
         super(name, surname, phone);
         this.email = email;
     }
@@ -14,6 +14,19 @@ public class MerchantA extends  Merchant {
         String surname = getSurname();
         String phone = getPhone();
         return "\nMerchant: " + name + " " + surname + ", " + phone + ", " + email + "\n";
+    }
+
+    @Override
+    public double calcBonus(double sales) {
+        if (sales <= 10000) {
+            return sales * 5 / 100;
+        } else if (sales > 10000 && sales <= 20000) {
+            return sales * 7.5 / 100;
+        } else if (sales > 20000 && sales <= 30000) {
+            return sales * 10 / 100;
+        } else {
+            return sales * 15 / 100;
+        }
     }
 
 }
