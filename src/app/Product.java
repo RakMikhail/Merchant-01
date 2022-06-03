40ed312db444c50210692091e9828ea0492891f5
Mikhail Rak <misha.rak.2401@gmail.com>
2022-06-03 12:31:24 +0300

    add new class Product

---
+++ src/app/Product.java
@@ -0,0 +1,15 @@
+package app;
+
+//Class for describing the product`s.
+public class Product {
+
+    private final String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
